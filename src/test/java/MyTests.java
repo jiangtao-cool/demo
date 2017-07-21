d06d40df38611ce5ac0527246ec0004b351f49f2
hustme <hustme@126.com>
2017-07-21 23:24:23 +0800

    test

---
+++ src/test/java/MyTests.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.springframework.util.Assert;
+
+public class MyTests {
+
+    @Test
+    public void exampleTest() {
+        Assert.state(false, "xxxxhustme error test");
+    }
+
+}
